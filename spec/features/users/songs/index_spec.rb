a5add4518a375d575e7c48e240ceebe3593ef147
Nathan Lambertson <lambertones@gmail.com>
2024-05-24 08:10:25 -0500

    Further testing and add simple coverage to rails helper

--- spec/features/users/songs/index_spec.rb
+++ spec/features/users/songs/index_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe "user's songs index page", type: :feature do
     song1 = user.songs.create!(title: "Dino Fight", genre: "Rock", vibe: "Angry", description: "2 Dinos Battling with Guitars", tempo: 167, form: "AABA", lyrics: "none", instrumentation: "Guitar, Bass, Drums")
     song2 = user.songs.create!(title: "Milawalkay", genre: "Butt-Rock", vibe: "Sad", description: "Partners no more", tempo: 111, form: "AABAC", lyrics: "You and I were meant for bigger things", instrumentation: "Guitar, Bass, Drums, Sax")
 
+    expect(user.songs.count).to eq(2)
+    expect(song1.title).to eq("Dino Fight")
+    expect(song2.title).to eq("Milawalkay")
+    
     visit "/users/#{user.id}/songs"
-save_and_open_page
+
     expect(page).to have_content(song1.title)
     expect(page).to have_link(song1.title)
   end
