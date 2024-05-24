6cde61b1c00f571a785e7f04a5999d95c35a22b1
Nathan Lambertson <lambertones@gmail.com>
2024-05-24 08:07:36 -0500

    Finish simple user's songs index

--- spec/features/users/songs/index_spec.rb
+++ spec/features/users/songs/index_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe "user's songs index page", type: :feature do
     user = User.create!(name: "Nate", email: "nate@doink.com", password: "123boom", password_confirmation: "123boom")
     song1 = user.songs.create!(title: "Dino Fight", genre: "Rock", vibe: "Angry", description: "2 Dinos Battling with Guitars", tempo: 167, form: "AABA", lyrics: "none", instrumentation: "Guitar, Bass, Drums")
     song2 = user.songs.create!(title: "Milawalkay", genre: "Butt-Rock", vibe: "Sad", description: "Partners no more", tempo: 111, form: "AABAC", lyrics: "You and I were meant for bigger things", instrumentation: "Guitar, Bass, Drums, Sax")
-    
-    require 'pry'; binding.pry
+
+    visit "/users/#{user.id}/songs"
+save_and_open_page
+    expect(page).to have_content(song1.title)
+    expect(page).to have_link(song1.title)
   end
 end
