fe1e60b8a3faa3312c5ccb0e6088e730cfe732df
Nathan Lambertson <lambertones@gmail.com>
2024-05-24 07:51:38 -0500

    Start writing test for first basic index view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
   end
+
+  describe 'relationships' do
+    it { should have_many(:songs) }
+  end
 end
